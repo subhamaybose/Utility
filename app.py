61e033b4d8af729395b2d51aecf179ea83a1ea87
subhamaybose <subhamaybose@gmail.com>
2024-10-25 20:36:15 +0530

    bug fix

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import streamlit as st
 experiential_knowledge_generator_page = st.Page("pages/experiential-knowledge.py", title="Generate Experiential Knowledge", icon="💼")
 proposal_generator_page = st.Page("pages/proposal-generator.py", title="Generate Project Proposal", icon="🗂️")
 
-pg = st.navigation([experiential_knowledge_generator_page, proposal_generator_page, sitemap_generator_page])
+pg = st.navigation([experiential_knowledge_generator_page, proposal_generator_page])
 st.set_page_config(page_title="Utility - Manager", page_icon="🚀")
 pg.run()
